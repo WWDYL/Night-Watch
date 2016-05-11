37532a7d71a3cc5e54c5364351ccf091d3dfe166
BarryGates <1337762760@qq.com>
2016-05-11 12:17:57 +0800

    加入解析常见包头解析

--- src/PacketCapturer.java
+++ src/PacketCapturer.java
@@ -3,14 +3,7 @@ import org.jnetpcap.PcapIf;
 import org.jnetpcap.nio.JBuffer;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
-import org.jnetpcap.packet.format.XmlFormatter;
-import org.jnetpcap.protocol.lan.Ethernet;
-import org.jnetpcap.protocol.network.Ip4;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.nio.charset.UnsupportedCharsetException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,28 +16,8 @@ import java.util.List;
 public class PacketCapturer implements PcapPacketHandler<String> {
 
     public void nextPacket(PcapPacket packet, String user) {
-//        System.out.printf("\nReceived packet at %s caplen=%-4d len=%-4d\n",
-//                new Date(packet.getCaptureHeader().timestampInMillis()),
-//                packet.getCaptureHeader().caplen(),
-//                packet.getCaptureHeader().wirelen());
         PacketMatcher packetMatcher = PacketMatcher.getInstance();
         packetMatcher.handlePacket(packet);
-
-//        try {
-//            if (packet.hasHeader(ethernet)) {
-//                System.out.printf("ethernet.type = %X\n", ethernet.type());
-//                System.out.printf("ethernet.source = %s\n", new String(ethernet.source(), "ASCII"));
-//                System.out.printf("ethernet.destination = %s\n", new String(ethernet.destination(), "GBK"));
-//            }
-//
-//            if (packet.hasHeader(ip4)) {
-//                System.out.printf("ip.type = %d\n", ip4.type());
-//                System.out.printf("ip.source = %s\n", new String(ip4.source(), "GBK"));
-//                System.out.printf("ip.destination = %s\n", new String(ip4.destination(), "GBK"));
-//            }
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        }
 
     }
 
@@ -70,7 +43,7 @@ public class PacketCapturer implements PcapPacketHandler<String> {
             System.err.printf("Error while opening device for capture: %s\n", errBuf.toString());
             return;
         }
-        pcap.loop(5 * 1000, new PacketCapturer(), "123");
+        pcap.loop(5 * 1000, new PacketCapturer(), null);
 
         pcap.close();
     }
--- src/PacketMatcher.java
+++ src/PacketMatcher.java
@@ -1,11 +1,14 @@
 import org.jnetpcap.packet.JPacket;
 import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.protocol.lan.Ethernet;
+import org.jnetpcap.protocol.network.Arp;
 import org.jnetpcap.protocol.network.Icmp;
 import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Http;
 import org.jnetpcap.protocol.tcpip.Tcp;
 import org.jnetpcap.protocol.tcpip.Udp;
+
+import java.util.Date;
 
 /**
  * PacketMatcher 类
@@ -16,11 +19,10 @@ public class PacketMatcher {
     private static PacketMatcher pm;
     private Ethernet ethernet = new Ethernet();
     private Ip4 ip = new Ip4();
+    private Arp arp = new Arp();
     private Tcp tcp = new Tcp();
     private Udp udp = new Udp();
     private Icmp icmp = new Icmp();
-    private Http http = new Http();
-
 
     public static PacketMatcher getInstance() {
         if (pm == null) {
@@ -30,11 +32,18 @@ public class PacketMatcher {
     }
 
     public void handlePacket(JPacket packet) {
+        System.out.printf("\nReceived packet at %s caplen=%-4d len=%-4d\n",
+                new Date(packet.getCaptureHeader().timestampInMillis()),
+                packet.getCaptureHeader().caplen(),
+                packet.getCaptureHeader().wirelen());
         if (packet.hasHeader(ethernet)) {
             ethernetHandler(ethernet);
         }
         if (packet.hasHeader(ip)) {
             ip4Handler(ip);
+        }
+        else if (packet.hasHeader(arp)) {
+            arpHandler(arp);
         }
         if (packet.hasHeader(icmp)) {
             icmpHandler(icmp);
@@ -45,36 +54,53 @@ public class PacketMatcher {
         else if (packet.hasHeader(tcp)) {
             tcpHandler(tcp);
         }
-        if (packet.hasHeader(http)) {
-            httpHandler(http);
-        }
     }
 
     private void ethernetHandler(Ethernet ethernet) {
         String srcMac = FormatUtils.mac(ethernet.source());
         String dstMac = FormatUtils.mac(ethernet.destination());
-        System.out.println(srcMac + " " + dstMac);
+        int length = ethernet.getLength();
+        int offset = ethernet.getOffset();
+        System.out.println("ETHERNET: " + srcMac + " -> " + dstMac + " " + length + " " + offset);
     }
 
     private void ip4Handler(Ip4 ip) {
         String srcIp = FormatUtils.ip(ip.source());
         String dstIp = FormatUtils.ip(ip.destination());
-        System.out.println(srcIp + " " + dstIp);
+        int type = ip.type();
+        int length = ip.length();
+        int ttl = ip.ttl();
+        System.out.println("IP: " + srcIp + " " + dstIp + " " + type + " " + length + " " + ttl);
     }
 
+    private void arpHandler(Arp arp) {
+        int operation = arp.operation();
+        String srcMac = FormatUtils.mac(arp.sha());
+        String srcIp = FormatUtils.ip(arp.spa());
+        String dstMac = FormatUtils.mac(arp.tha());
+        String dstIp = FormatUtils.ip(arp.tpa());
+        System.out.printf("ARP: %d   %s %s %s %s\n", operation, srcMac, srcIp, dstMac, dstIp);
+    }
     private void icmpHandler(Icmp icmp) {
-
+        int type = icmp.type();
+        int code = icmp.code();
+        System.out.printf("ICMP: type: %d code: %d\n", type, code);
     }
 
     private void tcpHandler(Tcp tcp) {
         String srcPort = String.valueOf(tcp.source());
+        String dstPort = String.valueOf(tcp.destination());
+        boolean ack = tcp.flags_ACK();
+        boolean rst = tcp.flags_RST();
+        boolean syn = tcp.flags_SYN();
+        boolean fin = tcp.flags_FIN();
+        System.out.printf("TCP: %s -> %s %b %b %b %b\n", srcPort, dstPort, ack, rst, syn, fin);
     }
 
     private void udpHandler(Udp udp) {
-
-    }
-
-    private void httpHandler(Http http) {
-
+        String srcPort = String.valueOf(udp.source());
+        String dstPort = String.valueOf(udp.destination());
+        int length = udp.length();
+        System.out.printf("UDP: len: %d %s -> %s\n", length, srcPort, dstPort);
     }
 }
