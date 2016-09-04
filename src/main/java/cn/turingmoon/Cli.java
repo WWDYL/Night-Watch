edef65dd73c072a54e7917f5586d9fb80bb1c806
Barry Gates <1337762760@qq.com>
2016-09-04 13:04:02 +0800

    add server option

--- src/main/java/cn/turingmoon/Cli.java
+++ src/main/java/cn/turingmoon/Cli.java
@@ -2,12 +2,17 @@ package cn.turingmoon;
 
 import org.apache.commons.cli.*;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Cli {
     static Options options = new Options();
 
     static {
-        options.addOption("h","help", false, "The command help");
-        options.addOption("f","file", false, "filename");
+        options.addOption("h", "help", false, "The command help");
+        options.addOption("f", "file", false, "filename");
+        options.addOption("s", "server", false, "Express");
     }
 
     static void printHelp() {
@@ -24,8 +29,29 @@ public class Cli {
                 return;
             }
 
-            String filename = cl.getOptionValue("file");
+            String filename = cl.getOptionValue("server");
             System.out.println(filename);
+
+            Runtime run = Runtime.getRuntime();
+            try {
+                Process p = run.exec("ping baidu.com");
+                BufferedInputStream in = new BufferedInputStream(p.getInputStream());
+                BufferedReader inBr = new BufferedReader(new InputStreamReader(in));
+                String lineStr;
+                while ((lineStr = inBr.readLine()) != null)
+                    //获得命令执行后在控制台的输出信息
+                    System.out.println(lineStr);// 打印输出信息
+                //检查命令是否执行失败。
+                if (p.waitFor() != 0) {
+                    if (p.exitValue() == 1)//p.exitValue()==0表示正常结束，1：非正常结束
+                        System.err.println("命令执行失败!");
+                }
+                inBr.close();
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
